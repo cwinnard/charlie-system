881710dbe71e2327e64aec665854475f8b352a11
Charlie <ctwinnard@gmail.com>
2018-11-29 09:59:01 -0700

    using router for users

---
+++ app/server/routers/usersRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const { User } = require('./../../database/models/user');
+
+const usersRouter = express.Router();
+
+usersRouter.get('/new', (req, res) => {
+	const user = new User({
+		email: 'newuser@gmail.com',
+	});
+	user.save().then((doc) => {
+		res.send(doc.email).status(200);
+	});
+});
+
+module.exports = { usersRouter };
--- app/server/server.js
+++ app/server/server.js
@@ -1,26 +1,26 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-require('../database/connect');
 const { User } = require('../database/models/user');
+const { usersRouter } = require('./routers/usersRouter');
 
+// Create app
 const app = express();
 const port = 3000;
 
+// Configure app
 app.use(bodyParser.json({limit: '1000kb'}));
 
+// Connect to database
+require('../database/connect');
+
+// Index
 app.get('/', (req, res) => {
 	res.send('hello world').status(200);
 });
 
-app.get('/test', (req, res) => {
-	const user = new User({
-		email: 'ctwinnard@gmail.com',
-	});
-	user.save().then((doc) => {
-		res.send(doc.email).status(200);
-	});
-});
+// Routers
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
 	console.log(`Running on port ${port}`);
